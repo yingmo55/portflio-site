ef8b4c2acc17046e276496610c4671e3044e7c47
Ying <97061798+yingmo55@users.noreply.github.com>
2022-02-27 17:52:19 -0500

    Import hero section as component. Adjust style

---
+++ src/Components/Hero/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Hero.css';
+
+function Hero () {
+    return (
+        <section id="hero">
+            <div id="hero-deco"></div>
+                <div id="hero-text">
+                <h2>Hi, I am</h2>
+                <p id="heroDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed enim ut sem viverra aliquet eget. Bibendum est ultricies integer quis. Id donec ultrices tincidunt arcu non sodales.</p>
+            </div>
+        </section>
+    )
+}
+
+export default Hero;
