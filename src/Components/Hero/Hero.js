6316a5269fbcae1bfc6a7f63870bae2c8d6604db
Ying <97061798+yingmo55@users.noreply.github.com>
2022-02-27 21:38:17 -0500

    Change tags. Add style
    - Change Hero heading tag to h1
    - Add typing animation for name
    - Adjust font-size
    - Add responsive size changes

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -2,11 +2,17 @@ import React from 'react';
 import './Hero.css';
 
 function Hero () {
+
+    const name1 = "Ying";
+    const name2 = 'yi Mo';
+
     return (
         <section id="hero">
             <div id="hero-deco"></div>
                 <div id="hero-text">
-                <h2>Hi, I am</h2>
+                <h1>Hi, I am <br/>{name1}<p id='typingEffect'>{name2}</p><span id='underscore'>_</span>:<br/>
+                A Front-end developer.
+                </h1>
                 <p id="heroDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed enim ut sem viverra aliquet eget. Bibendum est ultricies integer quis. Id donec ultrices tincidunt arcu non sodales.</p>
             </div>
         </section>
