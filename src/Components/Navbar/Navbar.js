282d9aae94514cff90a5dc432398a8bde05727db
Ying <97061798+yingmo55@users.noreply.github.com>
2022-03-03 11:14:15 -0500

    Add toggle function to mobile nav. Adjust style
    - Add function to toggle display of the nav bar
    - Add ref and effect hook to get the current screen size and show nav bar
    - Adjust mobile nav style

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './Navbar.css';
 
 function Navbar () {
+  const ref = useRef(null);
+  const [isResize, setIsResize] = useState(null);
+  const navMenu =document.getElementById('navMenu');
+  const navUl = document.getElementById('navUl');
+  const burgerNav = document.getElementById('burgerNav');
+  const mobileNavBar = () => {
+      navMenu.style.display = navMenu.style.display === 'none' ? 'flex' : 'none';
+  }
+
+  const showNav = () => {
+    const width = ref.current.offsetWidth;
+    if (navMenu){
+      if(width < 800) {
+        navMenu.style.display = 'none';
+        burgerNav.style.display = 'block';
+      } else {
+        burgerNav.style.display = 'none';
+        navMenu.style.display = 'flex'
+      }
+    } }
+
+    // updates if the screen resizes
+    useEffect(() => {
+        window.addEventListener('resize', setIsResize);
+        showNav()
+        return(() => {
+            window.removeEventListener('resize', setIsResize);
+        })
+      }, [isResize, showNav])
+
     return (
-        <header id="header">
+        <header id="header" ref={ref}>
         <p id="siteLogo">yingyimo.dev</p>
         <nav>
-          <button id="burgerNav"></button>
+          <button id="burgerNav" onClick={mobileNavBar}></button>
           <div id="navMenu">
-            <ul>
+            <ul id='navUl'>
               <li><a className="navLink" href="/#">Home</a></li>
               <li><a className="navLink" href="#projectShowcase">Projects</a></li>
               <li><a className="navLink" href="#skills">Skills</a></li>
