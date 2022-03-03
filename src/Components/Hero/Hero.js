c242f5dabe8aa3c1e92e3780d0ce84635f03eeb4
Ying <97061798+yingmo55@users.noreply.github.com>
2022-03-02 22:48:01 -0500

    Add decorative background element. Add navigation buttons
    - Change hero section to grid layout
    - Add decorative background elements
    - Add animation for background elements
    - Add buttons

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -6,19 +6,24 @@ function Hero () {
     const name1 = "Ying";
     const name2 = 'yi Mo'; //for the typing animation
     const title = "A Front-end developer";
-    const shortbio = 'I mainly work with React.js, and I like making minimalistic websites with cool effects using CSS.';
+    const shortbio = 'I mainly work with React.js, and I like making minimalistic websites with cool effects using CSS. '
 
     return (
         <section id="hero">
             <div className='hero-content'>
-                <div id="hero-deco"></div>
                 <div id="hero-text">
                     <h1>Hi, I am <br/>{name1}<p id='typingEffect'>{name2}</p><span id='underscore'>_</span>:<br/>
                     {title}.
                     </h1>
                     <p id="heroDescription">{shortbio}</p>
                 </div>
+                <div id='hero-buttons'>
+                    <button>my works</button>
+                    <button>how to reach me</button>
+                </div>
             </div>
+        <div id='hero-background'></div>
+        <div id='hero-background2'></div>
         </section>
     )
 }
