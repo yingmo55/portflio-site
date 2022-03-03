44f9d1d8f86181af45f71f6a80d86c8383edc5ee
Ying <97061798+yingmo55@users.noreply.github.com>
2022-03-03 17:55:33 -0500

    Add Contact Page
    - Add ContactForm Component to Contact Page
    - Create Contact.css

---
+++ src/Pages/Contact/Contact.js
@@ -0,0 +1,13 @@
+import ContactForm from "../../Contact/ContactForm/ContactForm";
+
+ 
+function ContactPage() {
+    return (
+    <>
+        <ContactForm />
+    </>
+    );
+  }
+  
+  export default ContactPage;
+  
