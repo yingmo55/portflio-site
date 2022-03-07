4b8ca1291d63a4f2290d960044f13b0f5f10f8c5
Ying <97061798+yingmo55@users.noreply.github.com>
2022-03-06 22:59:55 -0500

    Adjust footer
    - Change footer color
    - Remove sitemap
    - Remove original social links. Replaced with SocialButton component

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,26 +1,35 @@
 import React from 'react';
-
-import NavLinks from '../../Util/NavLinks/NavLinks';
-
+import SocialButton from '../SocialButton/SocialButton';
 import './Footer.css';
-import linkedinLogo from '../../assets/icons/contact/linkedin.svg';
-import githubLogo from '../../assets/icons/contact/github.svg';
-import emailLogo from '../../assets/icons/contact/envelope.svg';
+
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 function Footer () {
 
+  const contactInfo = [{
+    src: faLinkedin,
+    desc: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/yingyimo/'
+  },
+  {
+    src: faGithub,
+    desc: 'GitHub',
+    link: 'https://github.com/yingmo55'
+  },
+  {
+    src: faEnvelope,
+    desc: 'Email',
+    link: 'mailto:hello@yingyimo.dev'
+  }]
+
+
     return (
         <footer id="footer">
-        <div>
-          <address>
-            <p><img className="contact-icon" src={githubLogo} alt="GitHub Logo" />GitHub: <a href="https://github.com/yingmo55">yingmo55</a></p>
-            <p><img className="contact-icon" src={linkedinLogo} alt="LinkedIn Logo" />LinkedIn: <a href="https://www.linkedin.com/in/yingyimo/" >Yingyi Mo</a></p>
-            <p><img className="contact-icon" src={emailLogo} alt="Email Logo" />email: hello@yingyimo.dev</p>
-          </address>
-        </div>
-        <nav id="sitemap">
-          <NavLinks />
-        </nav>
+          <h2>Stay in touch:</h2>
+          <div className='contact-icon-set'>
+            <SocialButton contactInfo={contactInfo} />
+          </div>
       </footer>
     )
 }
