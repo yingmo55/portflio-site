368dda7eb62c8b39c4bd8c63b489d627030a4b71
Ying <97061798+yingmo55@users.noreply.github.com>
2022-03-06 23:02:35 -0500

    Add new sub section to skills. Adjust skill tags style.
    - Add more skills in Util/SkillDetails/
    - Adjust skill tag styles by adding border, background color, fixed height, and margin
    - Add additional section to skills

--- src/Util/SkillDetails/SkillDetails.js
+++ src/Util/SkillDetails/SkillDetails.js
@@ -8,23 +8,28 @@ import reactLogo from '../../assets/icons/Skills/react.svg';
 import bootstrapLogo from '../../assets/icons/Skills/bootstrap.svg';
 
 
-const languageClass = new SkillDetails('Languages', 'I can program in...');
-
+const languageClass = new SkillDetails('Languages', 'I can code in...');
+languageClass.skills = ['JavaScript', javaScriptLogo];
 languageClass.skills = ['HTML', htmlLogo];
 languageClass.skills = ['CSS', cssLogo];
-languageClass.skills = ['JavaScript', javaScriptLogo];
 languageClass.skills = ['python', pythonLogo];
 
 const frameworkClass = new SkillDetails('Frameworks', 'I can use these framework...');
 frameworkClass.skills = ['React', reactLogo]
 frameworkClass.skills = ['Bootstrap', bootstrapLogo]
+frameworkClass.skills = ['Jest']
 
-const toolsClass = new SkillDetails('Tools', 'I use these tools and software...');
+const toolsClass = new SkillDetails('Tools', 'I use these tools for development...');
 toolsClass.skills = ['Git']
-toolsClass.skills = ['Visual Studio Code']
-toolsClass.skills = ['Adobe Photoshop']
-toolsClass.skills = ['Adobe InDesign']
-toolsClass.skills = ['Adobe XD']
+toolsClass.skills = ['GitHub']
+toolsClass.skills = ['Netlify']
+
+const softwareClass = new SkillDetails('Software', 'I use these Software...');
+softwareClass.skills = ['Visual Studio Code']
+softwareClass.skills = ['PostMan']
+softwareClass.skills = ['Adobe Photoshop']
+softwareClass.skills = ['Adobe InDesign']
+softwareClass.skills = ['Adobe XD']
 
 const language = {
     ...languageClass.getSkills(), 
@@ -41,4 +46,9 @@ const tools = {
     skills: toolsClass.skills
 }
 
-export {language, framework, tools};
+const Software = {
+    ...softwareClass.getSkills(),
+    skills: softwareClass.skills
+}
+
+export {language, framework, tools, Software} ;
