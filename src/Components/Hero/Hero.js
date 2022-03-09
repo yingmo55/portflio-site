1b019db320010bc0d422685f2d22ec906250e8b2
Ying <97061798+yingmo55@users.noreply.github.com>
2022-03-09 12:23:47 -0500

    Adjust Hero style
    - Add more animation
    - Adjust button
    - Adjust font style

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -15,25 +15,28 @@ function Hero () {
                     <h1>Hi, I am <br/>{name1}<p id='typingEffect'>{name2}</p><span id='underscore'>_</span>:<br/>
                     {title}.
                     </h1>
+                    <hr id='heroHr' />
                     <p id="heroDescription">{shortbio}</p>
                 </div>
                 <div id='hero-buttons'>
-                    <button>
+                    <button className='heroNavButtons'>
                         <a href='#projectShowcase'>
                             <img src={rightArrow} className='right-angle-brackets' alt='right arrow' />
                             my works
                         </a>
                     </button>
-                    <button>
-                        <a href='/#'>
+                    <button className='heroNavButtons'>
+                        <a href='/contact'>
                             <img src={rightArrow} className='right-angle-brackets' alt='right arrow' />
                             how to reach me
                         </a>
                     </button>
                 </div>
             </div>
-        <div id='hero-background'></div>
-        <div id='hero-background2'></div>
+                <div className='hero-deco' id='hero-background' role="presentation"></div>
+                <div className='hero-deco' id='hero-background2' role="presentation"></div>
+                <div className='hero-deco' id='hero-background3' role="presentation"></div>
+                <div className='bg-with-filter' role="presentation"></div>
         </section>
     )
 }
