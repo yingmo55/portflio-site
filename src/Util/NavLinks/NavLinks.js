820b9ed26f32cc45b3489bcc31a9367b81c2ac28
Ying <97061798+yingmo55@users.noreply.github.com>
2022-03-09 15:45:32 -0500

    Add NavLink to links
    - import NavLink
    - Add class for active links
    - Add style for active links

--- src/Util/NavLinks/NavLinks.js
+++ src/Util/NavLinks/NavLinks.js
@@ -1,13 +1,24 @@
 import React from "react";
 import './NavLinks.css';
+import { NavLink } from "react-router-dom";
 
 export default function NavLinks() {
     return (
         <ul id='nav-Links'>
-          <li><a activeClassName='active-link' className="navLink" href="/#">Home</a></li>
-          <li><a activeClassName='active-link' className="navLink" href="#projectShowcase">Projects</a></li>
-          <li><a activeClassName='active-link' className="navLink" href="#skills">Skills</a></li>
-          <li><a activeClassName='active-link' className="navLink" href="/contact">Contact</a></li>
+          <li>
+            <NavLink to="/#" 
+                    className={({ isActive }) => isActive ? "navLink active-link" : "navLink"} >
+            Home
+            </NavLink>
+          </li>
+          <li><a className="navLink" href="/#projectShowcase">Projects</a></li>
+          <li><a className="navLink" href="/#skills">Skills</a></li>
+          <li>
+            <NavLink to="/contact" 
+                     className={({ isActive }) => isActive ? "navLink active-link" : "navLink"} >
+              Contact
+            </NavLink>
+          </li>
         </ul>
     )
 }
