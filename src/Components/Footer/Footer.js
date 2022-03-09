4c0d69971dda51d384aa5ba8c2373991c595968e
Ying <97061798+yingmo55@users.noreply.github.com>
2022-03-09 13:30:58 -0500

    Adjust Footer style
    - Add color variables to :root in App.css
    - Adjust footer style
    - Add responsive design to footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,35 +1,16 @@
 import React from 'react';
 import SocialButton from '../SocialButton/SocialButton';
 import './Footer.css';
-
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { contactInfo } from '../../Util/socialIcons';
 
 function Footer () {
 
-  const contactInfo = [{
-    src: faLinkedin,
-    desc: 'LinkedIn',
-    link: 'https://www.linkedin.com/in/yingyimo/'
-  },
-  {
-    src: faGithub,
-    desc: 'GitHub',
-    link: 'https://github.com/yingmo55'
-  },
-  {
-    src: faEnvelope,
-    desc: 'Email',
-    link: 'mailto:hello@yingyimo.dev'
-  }]
-
-
     return (
         <footer id="footer">
-          <hr />
           <div className='contact-icon-set'>
             <SocialButton contactInfo={contactInfo} />
           </div>
+          <p>Made with 🍵 by Ying</p>
       </footer>
     )
 }
