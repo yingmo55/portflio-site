aaa29641561bdfaef415a7fd74be7fa43d6acad5
Ying <97061798+yingmo55@users.noreply.github.com>
2022-03-09 16:42:05 -0500

    Revamp buttons and background style
    - Move button content into an object and pass into the component
    - Move background to its own div and adjust style

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import './Hero.css';
-import rightArrow from '../../assets/icons/angle-right.svg';
+import NavButton from '../NavButton/NavButton';
 
 function Hero () {
     const name1 = "Ying";
     const name2 = 'yi Mo'; //for the typing animation
     const title = "A Front-end developer";
     const shortbio = 'I mainly work with React.js, and I like making minimalistic websites with cool effects using CSS. '
+
+    const heroNavs = [
+        {
+            link: '/#skills',
+            desc: 'about me'
+    }, 
+        {
+            link: '/#projectShowcase',
+            desc: 'my works'
+    }, 
+        {
+        link: '/contact',
+        desc: 'how to reach me'
+}, 
+]
 
     return (
         <section id="hero">
@@ -19,24 +34,17 @@ function Hero () {
                     <p id="heroDescription">{shortbio}</p>
                 </div>
                 <div id='hero-buttons'>
-                    <button className='heroNavButtons'>
-                        <a href='#projectShowcase'>
-                            <img src={rightArrow} className='right-angle-brackets' alt='right arrow' />
-                            my works
-                        </a>
-                    </button>
-                    <button className='heroNavButtons'>
-                        <a href='/contact'>
-                            <img src={rightArrow} className='right-angle-brackets' alt='right arrow' />
-                            how to reach me
-                        </a>
-                    </button>
+                    {heroNavs.map(element => 
+                        <NavButton className='heroNavButtons' key={element.desc} link={element.link} desc={element.desc}/> )
+                    }
                 </div>
             </div>
+            <div className='heroBG'>
                 <div className='hero-deco' id='hero-background' role="presentation"></div>
                 <div className='hero-deco' id='hero-background2' role="presentation"></div>
                 <div className='hero-deco' id='hero-background3' role="presentation"></div>
                 <div className='bg-with-filter' role="presentation"></div>
+            </div>
         </section>
     )
 }
