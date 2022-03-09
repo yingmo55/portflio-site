ff7ed0cdb408fd948a056d5ea5441d59ff368c74
Ying <97061798+yingmo55@users.noreply.github.com>
2022-03-09 14:59:02 -0500

    Merge App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 import LandingPage from './Pages/Landing/Landing';
+import ContactPage from './Pages/Contact/Contact';
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
       <main>
-      <LandingPage />
+        <Route path='/contact'>
+          <ContactPage />
+        </Route>
+        <Route exact path='/'>
+          <LandingPage />
+        </Route>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
