bf3b3be9adb320e7f95c76aa9928d9b5d85b64b4
Ying <97061798+yingmo55@users.noreply.github.com>
2022-03-09 12:25:43 -0500

    Adjust contact page

--- src/Components/Contact/ContactForm/ContactForm.js
+++ src/Components/Contact/ContactForm/ContactForm.js
@@ -8,17 +8,21 @@ export default function ContactForm () {
             <div className="infoRow">
                 <div className="infoCol">
                     <label htmlFor='contactName'>Name<span className="required-asterisk">*</span></label>
-                    <input type='text' id='contactName' name='name' placeholder="Name" required/>
+                    <input className='contactInput' type='text' id='contactName' name='name' placeholder="Name" required/>
                 </div>
                 <div className="infoCol">
                     <label htmlFor='contactEmail'>Email<span className="required-asterisk">*</span></label>
-                    <input type='text' id='contactEmail' name='email' placeholder="Email" required/>
+                    <input className='contactInput' type='text' id='contactEmail' name='email' placeholder="Email" required/>
                 </div>
             </div>
-            <label htmlFor='contactSubject'>Subject<span className="required-asterisk">*</span></label>
-            <input type='text' id='contactSubject' name='subject' placeholder="Subject" required/>
-            <label htmlFor='contactMessage'>Message<span className="required-asterisk">*</span></label>
-            <textarea id="contactMessage" name="message" rows="5" cols="33" placeholder="Your Message" required>
+            <label htmlFor='contactSubject'>
+                Subject <span className="required-asterisk">*</span>
+            </label>
+            <input className='contactInput' type='text' id='contactSubject' name='subject' placeholder="Subject" required/>
+            <label htmlFor='contactMessage'>
+                Message<span className="required-asterisk">*</span>
+            </label>
+            <textarea className='contactInput' id="contactMessage" name="message" rows="5" cols="33" placeholder="Your Message" required>
             </textarea>
             <input type='submit' />
         </form>
