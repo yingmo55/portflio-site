996ad3bb158e03a11ea02cbed75f67db14f0a96b
Ying <97061798+yingmo55@users.noreply.github.com>
2022-03-10 03:03:22 -0500

    Update ContactForm.js

--- src/Components/Contact/ContactForm/ContactForm.js
+++ src/Components/Contact/ContactForm/ContactForm.js
@@ -8,24 +8,24 @@ export default function ContactForm () {
             <div className="infoRow">
                 <div className="infoCol">
                     <label htmlFor='contactName'>Name<span className="required-asterisk">*</span></label>
-                    <input className='contactInput' type='text' id='contactName' name='name' placeholder="Name" required/>
+                    <input className='contactInput' type='text' id='contactName' name='contactName' placeholder="Name" required/>
                     <span className="focusBorder"></span>
                 </div>
                 <div className="infoCol">
                     <label htmlFor='contactEmail'>Email<span className="required-asterisk">*</span></label>
-                    <input className='contactInput' type='text' id='contactEmail' name='email' placeholder="Email" required/>
+                    <input className='contactInput' type='text' id='contactEmail' name='contactEmail' placeholder="Email" required/>
                     <span className="focusBorder"></span>
                 </div>
             </div>
             <label htmlFor='contactSubject'>
                 Subject <span className="required-asterisk">*</span>
             </label>
-            <input className='contactInput' type='text' id='contactSubject' name='subject' placeholder="Subject" required/>
+            <input className='contactInput' type='text' id='contactSubject' name='contactSubject' placeholder="Subject" required/>
             <span className="focusBorder"></span>
             <label htmlFor='contactMessage'>
                 Message<span className="required-asterisk">*</span>
             </label>
-            <textarea className='contactInput' id="contactMessage" name="message" rows="5" cols="33" placeholder="Your Message" required></textarea>
+            <textarea className='contactInput' id="contactMessage" name="contactMessage" rows="5" cols="33" placeholder="Your Message" required></textarea>
             <span className="focusBorder"></span>
             <input type='submit' />
         </form>
