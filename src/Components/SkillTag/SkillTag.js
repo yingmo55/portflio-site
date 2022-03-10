9f308bcf38e5678fe3f59ff8248e3256980c2aa9
Ying <97061798+yingmo55@users.noreply.github.com>
2022-03-09 19:22:41 -0500

    Format code to be more readable

--- src/Components/SkillTag/SkillTag.js
+++ src/Components/SkillTag/SkillTag.js
@@ -5,12 +5,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function SkillTag (props) {
     const skills = props.skills;
 
-    return skills.map((skill, index) => ( <div className="skill-tag" key={index} id={skill.name}> 
-        { skill.src && 
-            <FontAwesomeIcon icon={skill.src} size="2x" className='skill-icon' /> 
-        }
+    return skills.map((skill, index) => ( 
+        <div className="skill-tag" key={index} id={skill.name}> 
+            { skill.src && 
+                <FontAwesomeIcon icon={skill.src} size="2x" className='skill-icon' /> }
         <span>{skill.name}</span>
-    </div>) )
-}
+        </div>)
+        
+        )}
 
 export default SkillTag;
