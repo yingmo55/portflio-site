7a663c8100cb771950ffde7880b9e2772eaf5f7a
Ying <97061798+yingmo55@users.noreply.github.com>
2022-03-10 01:51:36 -0500

    Add contact page content and style.

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -4,10 +4,16 @@ import ContactForm from "../../Components/Contact/ContactForm/ContactForm";
 import './Contact.css';
 
 function ContactPage() {
+    const contactMessage = "I am available for hire currently!\n Alternatively, you can find my socials to the right (or above):"
+
     return (
     <section id='contactPage'>
         <ContactContent />
-        <ContactForm />
+        <div className='contactMain'>
+            <h2>Contact</h2>
+            <p>{contactMessage}</p>
+            <ContactForm />
+        </div>
     </section>
     );
   }
