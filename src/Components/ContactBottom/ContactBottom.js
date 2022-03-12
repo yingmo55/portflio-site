686822aa88cee7c6da0e63c88704e13d322f22f4
Ying <97061798+yingmo55@users.noreply.github.com>
2022-03-12 16:36:01 -0500

    Create file

---
+++ src/Components/ContactBottom/ContactBottom.js
@@ -0,0 +1,10 @@
+import React from "react";
+import '.ContactBottom.js';
+
+function ContactFooter() {
+    return (
+        <section className='footer-contact'>
+            <p>Interested in woring with me?</p>
+        </section>
+    )
+}
